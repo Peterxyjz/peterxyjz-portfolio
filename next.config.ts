84b68d1e49c4eea9cac7b189302b86ab694bbb8d
peterxyjz <huyq09@gmail.com>
2025-05-13 03:31:09 +0700

    feat: ui homepage

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    appDir: true
-  },
-  reactStrictMode: true,
-  poweredByHeader: false
 };
 
 export default nextConfig;
